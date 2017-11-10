138f47d392313751a76472f62634364ff39cb5a4
Yuyuan <yuyuan.xiong@outlook.com>
2017-11-10 21:48:34 +0800

    Add files via upload

--- js/sample.js
+++ js/sample.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     
+
        //smooth scroll
   $("header a").on('click', function(event){
       if (this.hash !== "") {
@@ -33,9 +34,8 @@ $('a[rel=popover]').popover({
     
 });
     
-    
-   //back-to-top
-    $(window).on('scroll', function(){
+//back-to-top
+$(window).on('scroll', function(){
         if($(this).scrollTop() + $(window).height() > $(document).height() - $("#footer").height()){
             $('#back-to-top').css('visibility', 'visible');   
         }else{
@@ -43,12 +43,50 @@ $('a[rel=popover]').popover({
         };
     });
     
+//feedback 
     
+    $('.plane').on('click', function () {
+        
+        var userName = $('.username').val();
 
+        var userPhone = $('.userphone').val();
+        
+        if(userName === ''){
+            alert('姓名不能为空！')
+        }else if (userPhone === ''){
+            alert('联系信息不能为空！')
+        }else if(userName !== ''&& userPhone !== ''){
+            var data = {
+            'userName': userName,
+            'company': '',
+            'email': '',
+            'content': userPhone
+        };
+
+            $.ajax({
+               type: 'post',
+               url: '/api/feedback',
+               headers:{'Content-Type':'application/json'},
+                data: JSON.stringify(data),
+                dataType: 'json',
+                cache:false,
+                success: (res) => {
+                    alert('信息发送成功，我们会尽快与您联系！')
+                    $('.username,.userphone').val('')
+                },
+                error: (res) => {
+                    console.log(res.errorMessage)
+                }
+            })
+                 
+        };
+
+    })
     
     
     
-});
+    
+})
         
     
       
